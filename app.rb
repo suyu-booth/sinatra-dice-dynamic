28d879e45eea24da279646278f31b2cbbe78b88f
suyu-booth <suyu@chicagobooth.edu>
2024-10-25 21:23:54 -0500

    Update app.rb

--- app.rb
+++ app.rb
@@ -5,50 +5,59 @@ get("/") do
   erb(:homepage)
 end
 
-get("/dice/2/6") do
+get("/dynamic/:number_of_dice/:how_many_sides") do
+  @num_dice = params.fetch("number_of_dice").to_i
+
+  @sides = params.fetch("how_many_sides").to_i
+
   @rolls = []
 
-  2.times do
-    die = rand(1..6)
+  @num_dice.times do
+    die = rand(1..@sides)
 
     @rolls.push(die)
   end
 
-  erb(:two_six)
+  erb(:flexible)
+end
+
+get("/dice/2/6") do
+  first_die = rand(1..6)
+	second_die = rand(1..6)
+  sum = first_die + second_die
+	
+	@outcome = "You rolled a #{first_die} and a #{second_die} for a total of #{sum}."
+	
+	erb(:two_six)
 end
 
 get("/dice/2/10") do
-  @rolls = []
-
-  2.times do
-    die = rand(1..10)
-
-    @rolls.push(die)
-  end
-
+  first_die = rand(1..10)
+	second_die = rand(1..10)
+  sum = first_die + second_die
+	
+	@outcome = "You rolled a #{first_die} and a #{second_die} for a total of #{sum}."
+	
   erb(:two_ten)
 end
 
 get("/dice/1/20") do
-  @rolls = []
-
-  1.times do
-    die = rand(1..20)
-
-    @rolls.push(die)
-  end
-
-  erb(:one_twenty)
+  @die = rand(1..20)
+	
+	@outcome = "You rolled a #{@die}."
+	
+	erb(:one_twenty)
 end
 
 get("/dice/5/4") do
-  @rolls = []
-
-  5.times do
-    die = rand(1..4)
-
-    @rolls.push(die)
-  end
-
+  first_die = rand(1..4)
+	second_die = rand(1..4)
+  third_die = rand(1..4)
+  fourth_die = rand(1..4)
+  fifth_die = rand(1..4)
+  sum = first_die + second_die + third_die + fourth_die + fifth_die
+	
+	@outcome = "You rolled a #{first_die}, #{second_die}, #{third_die}, #{fourth_die}, and #{fifth_die} for a total of #{sum}."
+	
   erb(:five_four)
 end
